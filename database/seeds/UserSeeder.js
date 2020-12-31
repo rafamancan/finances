6cf5eafbed1a25c6c3c1fa758d192107c4048d24
rmancan <rafael.mancan@gmail.com>
2020-12-30 21:49:02 -0300

    creating user seeder

---
+++ database/seeds/UserSeeder.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| UserSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const Database = use("Database");
+const Hash = use("Hash");
+
+class UserSeeder {
+  async run() {
+    await Database.table("users").insert([
+      {
+        name: "Admin",
+        email: "admin@17bits.com.br",
+        password: await Hash.make("123123"),
+      },
+    ]);
+
+    const users = await Database.table("users");
+  }
+}
+
+module.exports = UserSeeder;
