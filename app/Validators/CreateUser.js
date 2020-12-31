3ca14a33ca192af5eaba8d5516ce82997dfe531b
rmancan <rafael.mancan@gmail.com>
2020-12-31 15:11:41 -0300

    fix(validators): createUser
    remove replace messages

--- app/Validators/CreateUser.js
+++ app/Validators/CreateUser.js
@@ -7,14 +7,6 @@ class CreateUser {
     };
   }
 
-  get messages() {
-    return {
-      required: '{{ field }} is required',
-      email: '{{ field }} is not valid e-mail',
-      string: '{{ field }} is not a valid string',
-    };
-  }
-
   get validateAll() {
     return true;
   }
